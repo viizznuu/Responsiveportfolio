e3ed742e76a72e7d28a0ab7edba7b90da5578d59
Vishnu VK <143687998+viizznuu@users.noreply.github.com>
2024-01-14 00:53:38 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Vishnu VK </span> Driven by a curiosity for cutting-edge technologies, I brings a wealth of enthusiasm to the intersection of AI and web development. As a student, that has not only acquired theoretical expertise but has also actively engaged in hands-on AI projects, showcasing a commitment to real-world applications. Eager to embark on a professional journey, I'm on the lookout for roles that foster continuous learning and provide a platform to apply skills in creating impactful solutions. With a solid foundation and a passion for innovation, I'm ready to contribute to the ever-evolving landscape of artificial intelligence and machine learning."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
